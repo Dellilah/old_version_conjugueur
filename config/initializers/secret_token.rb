4e5c758b86f242986f206714612afd037ad9dbff
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-09-10 09:48:33 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Conjugeur::Application.config.secret_key_base = '9d22fa0dd338565dedbec88423d07361c1ef37716bfa4697bc32d436f7c6418c6bd581dfad5b5eae3efd25e7d3c81383d9f18e3f09c3b1b16891ac4c111dd40d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Conjugeur::Application.config.session_store :cookie_store, key: '_Conjugeur_session'
