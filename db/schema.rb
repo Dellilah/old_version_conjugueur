3870a3f1ad938733947195979e1daccc14950ebe
Alicja Cyganiewicz <acyganiewicz@gmail.com>
2013-09-25 14:34:20 +0200

    resigning from angular

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130916093051) do
+ActiveRecord::Schema.define(version: 20130910081115) do
 
   create_table "futur_simples", force: true do |t|
     t.string   "je"
